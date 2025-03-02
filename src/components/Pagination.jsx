00493662fd002efe70afcf621bef9f247f3e35e7
Priyanka <priyankagjune@gmail.com>
2025-03-02 10:13:06 -0700

    changed pagination on main page

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,28 +7,6 @@ export const Pagination = ({ productPerPage, totalProducts, paginate }) => {
   return (
     <div className="flex justify-center">
       <nav class="flex items-center gap-x-1" aria-label="Pagination">
-        <button
-          type="button"
-          class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-          aria-label="Previous"
-        >
-          <svg
-            aria-hidden="true"
-            class="hidden shrink-0 size-3.5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="m15 18-6-6 6-6"></path>
-          </svg>
-          <span>Previous</span>
-        </button>
         <ul className="flex items-center gap-x-1">
           {pageNumbers.map((num) => (
             <li key={num}>
@@ -41,28 +19,6 @@ export const Pagination = ({ productPerPage, totalProducts, paginate }) => {
             </li>
           ))}
         </ul>
-        <button
-          type="button"
-          class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-          aria-label="Next"
-        >
-          <span>Next</span>
-          <svg
-            aria-hidden="true"
-            class="hidden shrink-0 size-3.5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="m9 18 6-6-6-6"></path>
-          </svg>
-        </button>
       </nav>
     </div>
   );
